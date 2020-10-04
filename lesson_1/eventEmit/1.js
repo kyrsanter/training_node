12b7278dcb3cdd05776d652e65fd4627058daa5d
Vitalii Rybachuk <vitalii.rybachuk@infopulse.com>
2020-10-04 14:42:16 +0300

    add(in lesson_1/eventEmitt) first example EventEmitter

---
+++ lesson_1/eventEmit/1.js
@@ -0,0 +1,18 @@
+const {EventEmitter} = require('events');
+
+const storage = new EventEmitter();
+
+storage.on('data', (data) => {   // calls first with the same event name "data"
+	if (data.age > 25) {
+		data.prop = 'old'
+	}
+})
+
+storage.on('data', (data) => { // calls second with the same event name "data"
+	console.log(data) // { name: 'Vasya', age: 26, prop: 'old' }
+})
+
+storage.emit('data', {
+	name: 'Vasya',
+	age: 26
+});
